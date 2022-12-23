b06072e9f4622aca75199625506674ecb70bf7e8
Vitor Buxbaum Orlandi <vitor.buxbaum@gmail.com>
2022-12-22 22:16:49 -0300

    Update pytest_dependency.py

--- src/pytest_dependency.py
+++ src/pytest_dependency.py
@@ -1,11 +1,6 @@
-"""pytest-dependency - Manage dependencies of tests
+"""$DOC"""
 
-This pytest plugin manages dependencies of tests.  It allows to mark
-some tests as dependent from other tests.  These tests will then be
-skipped if any of the dependencies did fail or has been skipped.
-"""
-
-__version__ = "UNKNOWN"
+__version__ = "$VERSION"
 
 import logging
 import pytest
